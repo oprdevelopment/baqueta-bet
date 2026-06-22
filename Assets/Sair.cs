6431f7528ffa6ace6cc17423986839b3279ba18b
ballanienzo <ballanienzo07@gmail.com>
2026-06-22 18:43:40 -0300

    play again

--- Assets/Sair.cs
+++ Assets/Sair.cs
@@ -1,11 +1,22 @@
+using Assets.Bet.UI;
 using UnityEngine;
+using UnityEngine.UIElements;
 using UnityEngine.Video;
 
 public class Sair : MonoBehaviour
 {
     void Awake()
     {
-        Cursor.visible = false;
-        GetComponent<VideoPlayer>().loopPointReached += e => Application.Quit();
+        var root = FindAnyObjectByType<UIDocument>().rootVisualElement;
+        root.Display(false);
+        UnityEngine.Cursor.visible = false;
+        GetComponent<VideoPlayer>().loopPointReached += e =>
+        {
+            root.Q<Button>("Again").clicked += () => UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+            root.Q<Button>("Sair").clicked += () => Application.Quit();
+            root.Display(true);
+            UnityEngine.Cursor.visible = true;
+            UnityEngine.Cursor.lockState = CursorLockMode.Confined;
+        };
     }
 }
